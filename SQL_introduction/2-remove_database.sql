0e2a11389bdb4db59a7350029cb5d7ca4d92ef12
Kietimartin <martinkieti6@gmail.com>
2023-08-17 20:42:36 +0300

    Dropping a database

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,3 +1,4 @@
 -- This SQL code removes the database hbtn_0c_0 from the MYSQL server if it exists
+-- if it exists we drop it
 
-DROP DATABASE [IF NOT EXISTS] hbtn_0c_0;
+DROP DATABASE IF EXISTS hbtn_0c_0;
