7976e6876dac5b0198e7955bf336bcf4cd537595
Kietimartin <martinkieti6@gmail.com>
2023-08-17 21:13:42 +0300

    first add

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,7 +1,7 @@
 -- This script should;
 -- insert a new row to the first_table
 -- values ; id = 89
---          name  = Holbertron School
+--          name  = Holberton School
 
 
-INSERT INTO first_table VALUES (89,'Holbertron School');
+INSERT INTO first_table VALUES (89,'Holberton School');
