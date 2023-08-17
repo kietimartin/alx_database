935a5a81a1e3b8c18bda220ffcce37b95044f0ab
Kietimartin <martinkieti6@gmail.com>
2023-08-17 22:07:30 +0300

    Full creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -4,9 +4,7 @@
 -- id, name, score
 -- create several records
 
-CREATE TABLE IF NOT EXISTS second_table (id INT,
-    name VARCHAR(256),
-    score INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 
 --INSERTING THE RECORDS
 INSERT INTO second_table (id, name, score) VALUES (1, "John", 10), 
