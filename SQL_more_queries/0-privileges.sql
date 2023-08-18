c03029bfb0eedc458116f8270b92c8088d021465
kietimartin <martinkieti6@gmail.com>
2023-08-18 12:17:30 -0700

    More Queries

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- this code lists all privileges of the users on your server
+
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- creates a database and a user
+-- the user has only SELECT privilege
+-- user password - user_0d_2_pwd
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
